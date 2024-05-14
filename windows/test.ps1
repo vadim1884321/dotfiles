fa523b2265faa5d2da21955397a42f52d95e2efd
vadim <vadim1884321@gmail.com>
2024-05-14 16:58:16 +0300

    ♻️ Automatic Update

--- windows/test.ps1
+++ windows/test.ps1
@@ -1,5 +1,5 @@
 #Requires -RunAsAdministrator
 
-. "$PSScriptRoot\helpers.ps1"
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vadim1884321/dotfiles/main/windows/helpers.ps1'))
 
 Write-Section -Text "Test installing..." -Color Green
